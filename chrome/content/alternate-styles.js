3fa8fe2d2b5986d08762d04018ebad7344b7cd1a
Simon Lindholm <simon.lindholm10@gmail.com>
2014-04-05 14:44:25 +0200

    initial commit

---
+++ chrome/content/alternate-styles.js
@@ -0,0 +1,38 @@
+var lb2_alternateStyles = {
+  styles: ["domain-color","domain-strong","subdomain-inherit","pathFile-inherit","go-persist","segments-margin","breadcrumb-domain","breadcrumb-all"],
+  styleSheets: {},
+  prefs: Components.classes["@mozilla.org/preferences-service;1"]
+                   .getService(Components.interfaces.nsIPrefService)
+                   .getBranch("extensions.locationbar2.alternate.")
+                   .QueryInterface(Components.interfaces.nsIPrefBranch2),
+  init: function () {
+    this.prefs.addObserver("", this, false);
+    var ss = document.styleSheets;
+    for (var i = ss.length - 1; i >= 0; i--) {
+      var style = ss[i].href.match(/^chrome:\/\/locationbar2\/skin\/alternate\/(.+)\.css$/);
+      if (style) {
+        this.styleSheets[style[1]] = ss[i];
+        this.handlePref(style[1]);
+      }
+    }
+  },
+  observe: function (subject, topic, data) {
+    this.handlePref(data);
+  },
+  handlePref: function (style) {
+    if (style == "domain-color")
+      this.styleSheets[style].cssRules[1].style.setProperty("color", this.prefs.getCharPref(style) || "inherit", "");
+    else
+      this.styleSheets[style].disabled = !this.prefs.getBoolPref(style);
+  },
+  uninit: function() {
+    this.prefs.removeObserver("", this);
+  }
+};
+
+window.addEventListener("load", function() {
+  lb2_alternateStyles.init();
+}, false);
+window.addEventListener("unload", function() {
+  lb2_alternateStyles.uninit();
+}, false);
---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,14 @@
+pref("extensions.locationbar2@design-noir.de.description", "chrome://locationbar2/locale/locationbar2.properties");
+pref("extensions.locationbar2@design-noir.de.homepageURL", "chrome://locationbar2/locale/locationbar2.properties");
+pref("browser.urlbar.hideProtocols", "");
+pref("browser.urlbar.mouseOnTopLinkifies", false);
+pref("browser.urlbar.mouseOnBottomLinkifies", false);
+
+pref("extensions.locationbar2.alternate.domain-strong", false);
+pref("extensions.locationbar2.alternate.domain-color", "");
+pref("extensions.locationbar2.alternate.subdomain-inherit", false);
+pref("extensions.locationbar2.alternate.pathFile-inherit", false);
+pref("extensions.locationbar2.alternate.go-persist", false);
+pref("extensions.locationbar2.alternate.segments-margin", false);
+pref("extensions.locationbar2.alternate.breadcrumb-domain", false);
+pref("extensions.locationbar2.alternate.breadcrumb-all", false);
